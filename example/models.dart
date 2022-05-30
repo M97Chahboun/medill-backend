8655e4e3eb5b4680e36d4ad3844077b476197acf
Mohammed CHAHBOUN <m97.chahboun@gmail.com>
2022-05-30 01:15:19 +0100

    update dart to 2.17.0 & migrate code

--- example/models.dart
+++ example/models.dart
@@ -3,19 +3,18 @@ import 'package:medill/medill.dart';
 class Developers extends Model {
   INTEGER? age;
   TEXT? language;
-
-  Developers(String table) : super(table);
+  Developers(super.table);
 }
 
 class Managers extends Model {
   TEXT? name;
 
-  Managers(String table) : super(table);
+  Managers(super.table);
 }
 
 class Seniors extends Model {
   INTEGER? years;
   TEXT? company;
 
-  Seniors(String table) : super(table);
+  Seniors(super.table);
 }
